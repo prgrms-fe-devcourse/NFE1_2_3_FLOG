2841d33ed73afe606d72212937a1d9a79e067524
w-ho-choo <ettoprivate@gmail.com>
2024-10-23 12:17:59 +0900

    feat: 인기검색어 컴포넌트 마크업 제작

--- frontend/src/pages/main/MainPage.tsx
+++ frontend/src/pages/main/MainPage.tsx
@@ -1,9 +1,44 @@
+import styled from "styled-components";
 import PostItem from "../../shared/components/postItem/PostItem";
+import TopSearched from "../../shared/components/TopSearched/TopSearched";
+
+const MainWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 1304px;
+  margin: 0 auto;
+`
+
+const MainPostWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`
+
+const MainSideBar = styled.section`
+  width: 309px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 70px;
+`
 
 const MainPage = () => {
   return (
     <main>
-      <PostItem />
+      <MainWrapper>
+        <MainPostWrapper>
+          <PostItem />
+        </MainPostWrapper>
+
+        <MainSideBar>
+          <TopSearched></TopSearched>
+        </MainSideBar>
+      </MainWrapper>
     </main>
   );
 };
