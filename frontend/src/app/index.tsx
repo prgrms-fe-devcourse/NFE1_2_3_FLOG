a22f90178a91cd5626c236294cd4cfe814215f34
LimJaeHyun <guswogus81@gmail.com>
2024-10-23 16:31:17 +0900

    feat: 회원가입 유효성 검사 기능 추가

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom"; // useNavigate 사용하기 위해 추가 1
 
-const rootElement = document.getElementById('app');  // 'root' -> 'app' 으로 변경
+const rootElement = document.getElementById("app"); // 'root' -> 'app' 으로 변경
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement as HTMLElement);
   root.render(
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>
   );
 } else {
-  console.error('App element not found');
+  console.error("App element not found");
 }
-
