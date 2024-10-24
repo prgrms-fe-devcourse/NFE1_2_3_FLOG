b117c55c663f19116d837382fbd309ba416b33ae
LimJaeHyun <guswogus81@gmail.com>
2024-10-24 15:08:40 +0900

    chore : react-router-dom 라이브러리 추가

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,18 +1,20 @@
-import './index.css'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const rootElement = document.getElementById('app');  // 'root' -> 'app' 으로 변경
+const rootElement = document.getElementById("app"); // 'root' -> 'app' 으로 변경
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement as HTMLElement);
   root.render(
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>
   );
 } else {
-  console.error('App element not found');
+  console.error("App element not found");
 }
-
