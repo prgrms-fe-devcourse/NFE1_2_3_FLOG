b14e8beb816e8edd9e7fe8a204b8183aff258519
LimJaeHyun <guswogus81@gmail.com>
2024-10-25 14:12:50 +0900

    feat : 로그인 페이지 마크업

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SignupPage from "../pages/auth/SignupPage";
 import { Route, Routes } from "react-router-dom";
+import SigninPage from "../pages/auth/SigninPage";
 
 function App() {
   return (
@@ -9,10 +10,7 @@ function App() {
         <Route path="/" element={<div>메인페이지</div>} />
         <Route path="/detail" element={<div>디테일페이지</div>} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route
-          path="/signin"
-          element={<div>로그인 페이지입니다 (수정 예정)</div>}
-        />
+        <Route path="/signin" element={<SigninPage />} />
       </Routes>
     </div>
   );
