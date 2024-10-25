b68220536f96568ada9d992411e518f431471a38
ahcgnoej <jinjinchachajung@naver.com>
2024-10-25 14:56:16 +0900

    feat: 마크업 및 상세,마이페이지 일부 기능구현

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+
+const useStore = create((set) => ({
+  isModalOpen: false,
+  openModal: () => set({ isModalOpen: true }),
+  closeModal: () => set({ isModalOpen: false }),
+}));
+
+export default useStore;
--- frontend/src/pages/user/MyPage.tsx
+++ frontend/src/pages/user/MyPage.tsx
@@ -2,24 +2,29 @@ import styled from "styled-components";
 import MyPageItem from "../../features/mypage/MyPageItem";
 import MyPageProfile from "../../features/mypage/MyPageProfile";
 import MyPageAuth from "../../features/mypage/MyPageAuth";
+import MyPageHeader from "../../features/mypage/MyPageHeader";
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+  // background-color: #cccccc;
+  width: 864px;
+  margin-top: 50px;
+`;
 
 const MyPage = () => {
-  const Box = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    margin: 0 auto;
-    // background-color: #cccccc;
-    width: 864px;
-    margin-top: 50px;
-  `;
+  //내 페이지인지 다른 유저 페이지인지 확인하는 로직 필요
+  const mypage = true;
+
   return (
     <div>
-      <header>공통 컴포넌트</header>
+      <MyPageHeader></MyPageHeader>
       <Box>
         <MyPageProfile />
         <MyPageItem />
-        <MyPageAuth />
+        {mypage && <MyPageAuth />}
       </Box>
     </div>
   );
