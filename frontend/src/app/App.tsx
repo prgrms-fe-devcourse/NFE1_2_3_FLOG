71541dc5f5f8160631fe8ae5f8650e11bb1694fe
LimJaeHyun <guswogus81@gmail.com>
2024-10-28 09:48:33 +0900

    feat : 포스트 생성 라우터 설정

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import SignupPage from "../pages/auth/SignupPage";
 import { Route, Routes } from "react-router-dom";
 import SigninPage from "../pages/auth/SigninPage";
+import PostCreateEditPage from "../pages/post/PostCreateEditPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/detail" element={<div>디테일페이지</div>} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/signin" element={<SigninPage />} />
+        <Route path="/post/create" element={<PostCreateEditPage />} />
       </Routes>
     </div>
   );
