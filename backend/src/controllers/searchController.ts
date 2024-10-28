d84fb8534694785d86005f3141935a9195145595
w-ho-choo <ettoprivate@gmail.com>
2024-10-28 12:11:23 +0900

    feat: 검색어 쿼리 설정

--- backend/src/controllers/searchController.ts
+++ backend/src/controllers/searchController.ts
@@ -5,10 +5,12 @@ import { SearchLog } from "../models/searchlogModel";
 export const searchPosts = async (req: Request, res: Response) => {
   const { query, gender, age, style } = req.query;
 
+  // 검색어는 쿼리 옵션은 대소문자 구문없이 (맞나...?)
   const searchConditions: any = {
     title: { $regex: query, $options: "i" }
   }
 
+  // 키워드 걸기
   if (gender && gender !== '전체') {
     searchConditions.genderFilter = gender
   }
@@ -22,6 +24,18 @@ export const searchPosts = async (req: Request, res: Response) => {
   }
 
   try {
+    // 쿼리값으로 대조하여 검색어 기록남기기
+    if (query) {
+      const existingLog = await SearchLog.findOne({ query });
+      if(existingLog) {
+        existingLog.searchCount += 1;
+        await existingLog.save();
+      } else {
+        const newLog = new SearchLog({ query, searchCount: 1, createdAt: new Date()})
+        await newLog.save();
+      }
+    }
+
     const posts = await Post.find(searchConditions).populate("authorId", "nickname profileImage")
     res.status(200).json({ posts })
   } catch (err) {
@@ -31,7 +45,7 @@ export const searchPosts = async (req: Request, res: Response) => {
 
 export const getTrendingSearches = async (req: Request, res: Response) => {
   try {
-    const trendingSearches = await SearchLog.find().sort({ searchCount: -1 }).limit(10);
+    const trendingSearches = await SearchLog.find().sort({ searchCount: -1 }).limit(5);
     res.status(200).json({ trendingSearches });
   } catch (err) {
     res.status(500).json({ message: "서버 오류 발생", err })
