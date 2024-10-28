02ef8c1f4d6c1e0e5d8faf969f057dda93b12368
LimJaeHyun <guswogus81@gmail.com>
2024-10-28 15:29:17 +0900

    feat : 버튼 클릭 기능 구현

--- frontend/src/features/posts/postcreateedit/PostCreateButtons.tsx
+++ frontend/src/features/posts/postcreateedit/PostCreateButtons.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import axios from "axios";
+import usePostCreateStore from "./PostCreateStore";
+
+const pink = "#F9F4F4";
+const black = "#212529";
+const gray = "#7D7D7D";
 
 const ButtonBox = styled.div`
   width: 1000px;
@@ -21,15 +28,66 @@ const Button = styled.button`
   width: 100px;
   height: 40px;
   cursor: pointer;
+  background-color: ${pink};
+  border: 1px solid ${gray};
 `;
 
 const PostCreateButtons = () => {
+  const navigate = useNavigate();
+  const { data, setData } = usePostCreateStore();
+
+  // 서버로 POST 요청하는 함수
+  const postData = async () => {
+    try {
+      const res = await axios.post("api/posts/create", data, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // "출간하기" 버튼 클릭 시 status:"published" 상태로 전달
+  const postCreateButton = () => {
+    setData({ ...data, status: "published" });
+    console.log(data);
+    postData();
+  };
+
+  // "임시저장" 버튼 클릭 시 status:"draft" 상태로 전달
+  const temporarySave = () => {
+    setData({ ...data, status: "draft" });
+    postData();
+  };
+
   return (
     <ButtonBox>
-      <Button>나가기</Button>
+      <Button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        나가기
+      </Button>
       <RightButtons>
-        <Button>임시저장</Button>
-        <Button>출간하기</Button>
+        <Button
+          onClick={() => {
+            temporarySave();
+          }}
+        >
+          임시저장
+        </Button>
+        <Button
+          onClick={() => {
+            postCreateButton();
+            navigate("/"); // 추후에 마이페이지 or 상세페이지로 이동
+          }}
+        >
+          출간하기
+        </Button>
       </RightButtons>
     </ButtonBox>
   );
