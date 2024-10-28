c61412845ad0bfe111b5faae2dd87159f26938b1
w-ho-choo <ettoprivate@gmail.com>
2024-10-28 11:18:51 +0900

    feat: 키워드 검색 기능

--- backend/src/controllers/searchController.ts
+++ backend/src/controllers/searchController.ts
@@ -3,10 +3,26 @@ import { Post } from "../models/postModel";
 import { SearchLog } from "../models/searchlogModel";
 
 export const searchPosts = async (req: Request, res: Response) => {
-  const { query } = req.query;
+  const { query, gender, age, style } = req.query;
+
+  const searchConditions: any = {
+    title: { $regex: query, $options: "i" }
+  }
+
+  if (gender && gender !== '전체') {
+    searchConditions.genderFilter = gender
+  }
+
+  if (age && age !== '전체') {
+    searchConditions.ageFilter = age
+  }
+
+  if (style && style !== '전체') {
+    searchConditions.styleFilter = style
+  }
+
   try {
-    const posts = await Post.find({ title: { $regex: query, $options: "i" } })
-      .populate("authorId", "nickname profileImage")
+    const posts = await Post.find(searchConditions).populate("authorId", "nickname profileImage")
     res.status(200).json({ posts })
   } catch (err) {
     res.status(500).json({ message: "서버 오류 발생", err })
