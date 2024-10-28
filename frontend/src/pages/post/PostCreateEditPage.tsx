e5ff0edf40b2335c6bed2d8d2a62d0dcfe11bd50
LimJaeHyun <guswogus81@gmail.com>
2024-10-28 09:49:01 +0900

    feat : 포스트 생성 페이지

--- frontend/src/pages/post/PostCreateEditPage.tsx
+++ frontend/src/pages/post/PostCreateEditPage.tsx
@@ -1,1 +1,44 @@
+import PostCreateTitle from "../../features/posts/postcreateedit/PostCreateTitle";
+import PostCreateTag from "../../features/posts/postcreateedit/PostCreateTag";
+import PostCreateCategory from "../../features/posts/postcreateedit/PostCreateCategory";
+import PostCreateEditor from "../../features/posts/postcreateedit/PostCreateEditor";
+import PostCreateButtons from "../../features/posts/postcreateedit/PostCreateButtons";
+import styled from "styled-components";
 
+// 가운데 정렬을 하기 위한 css 코드
+const PostCreateContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+
+// <PostCreateTag>와 <PostCreateCategory>를 한줄로 정렬하기 위한 css코드
+const TagCategoryBox = styled.div`
+  width: 1000px;
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  margin: 0 auto;
+  margin-top: 5px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const PostCreateEditPage = () => {
+  // PostCreateTitle : 제목을 입력받기 위한 인풋창
+  // PostCreateTag : 태그를 입력받기 위한 인풋창
+  //
+  return (
+    <PostCreateContent>
+      <PostCreateTitle />
+      <TagCategoryBox>
+        <PostCreateTag />
+        <PostCreateCategory />
+      </TagCategoryBox>
+      <PostCreateEditor />
+      <PostCreateButtons />
+    </PostCreateContent>
+  );
+};
+
+export default PostCreateEditPage;
