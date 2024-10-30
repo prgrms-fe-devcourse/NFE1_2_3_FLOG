240586e97a4c8ad31fc0efb5e586d92bed14dec1
w-ho-choo <ettoprivate@gmail.com>
2024-10-30 12:12:11 +0900

    feat: 포스트 리스트 fetch함수 추가, 포스트 리스트에 맞춰 무한 스크롤 되도록 변경

--- frontend/src/shared/components/postTemplate/PostTemplate.tsx
+++ frontend/src/shared/components/postTemplate/PostTemplate.tsx
@@ -3,9 +3,8 @@ import Search from "../search/Search";
 import Sort from "../search/Sort";
 import PostItem from "../postItem/PostItem";
 
-import React, { useEffect, useRef, useState } from "react";
-
-import { postData } from "../postItem/mockData";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import axios from "axios";
 
 interface PostTemplatePropTypes {
   postType: "post"  | "promotion" | "event" 
@@ -60,33 +59,37 @@ const SearchSortWrap = styled.div`
 
 const PostTemplate: React.FC<PostTemplatePropTypes> = ({ postType }) => {
 
-  const [postList, setPostList] = useState<PostDataTypes[]>(postData.slice(0, 4));
+  const [postData, setPostData] = useState<PostDataTypes[]>([])
+  const [postList, setPostList] = useState<PostDataTypes[]>([]);
   const [sortType, setSortType] = useState('new');
   const [hasMore, setHasMore] = useState(true);
-  const [page, setPage] = useState(4);
+  const [page, setPage] = useState(1);
 
   // 무한스크롤 Ref
-  const elementRef = useRef(null);
+  const elementRef = useRef<HTMLDivElement | null>(null);
 
   // 인터섹션 함수
-  const onIntersection = (entries: IntersectionObserverEntry[]) => {
+  const onIntersection = useCallback((entries: IntersectionObserverEntry[]) => {
     const firstEntry = entries[0]
 
     if (firstEntry.isIntersecting && hasMore) {
       loadMorePosts();
     }
-  }
+  }, [hasMore, postData, postList, page])
 
   // 포스트 로드함수
   const loadMorePosts = () => {
     // 더이상 로드할게 없을시 스크롤 종료, 그게 아니라면 로드
     // api에 맞게 수정예정
-    if (postData.length === postList.length) {
-      setHasMore(false)
-    } else {
-      setPostList((prev) => [...prev, ...postData.slice(page, page + 4)])
+    if (postData.length !== 0) {
+      if (postData.length === postList.length) {
+        setHasMore(false)
+      } else {
+        setHasMore(true)
+        setPostList((prev) => [...prev, ...postData.slice((page - 1) * 4, page * 4)])
 
-      setPage((prev) => prev + 4);
+        setPage((prev) => prev + 1);
+      }
     }
   }
 
@@ -95,11 +98,26 @@ const PostTemplate: React.FC<PostTemplatePropTypes> = ({ postType }) => {
     setSortType(value)
   }
 
+  // 리스트 받아오기 함수
+  const fetchPostList = async () => {
+    let listURL = 'http://localhost:5000/api/posts'
+    try {
+      const response = await axios.get<{ postList: PostDataTypes[] }>(listURL, {
+        params: { postType }
+      });
+      setPostData(response.data.postList)
+    } catch(err) {
+      console.error("API 호출 에러", err)
+    }
+  }
+
   // 무한스크롤 실행 구문
   useEffect(() => {
     const observer = new IntersectionObserver(onIntersection, {
       threshold: 1,
     });
+    console.log(observer)
+    console.log(elementRef)
 
     if (elementRef.current) {
       observer.observe(elementRef.current)
@@ -112,6 +130,17 @@ const PostTemplate: React.FC<PostTemplatePropTypes> = ({ postType }) => {
     }
   }, [onIntersection])
 
+  // 리스트 받아오기
+  useEffect(() => {
+    fetchPostList();
+  }, [postType])
+
+  // 받아온 리스트를 렌더링할 데이터로 정제
+  useEffect(() => {
+    setPostList(postData.slice(0, 4))
+    setPage(2)
+  }, [postData])
+
   return (
     <div>
       <PostTemplateRightWrap>
@@ -123,11 +152,12 @@ const PostTemplate: React.FC<PostTemplatePropTypes> = ({ postType }) => {
         </SearchSortWrap>
         <PostTemplatePostWrapper>
           {
-            postList && postList.map((post) => {
+            postList ? postList.map((post, index) => {
               return (
-                <PostItem post={post} key={post._id} />
+                <PostItem post={post} key={index} /> // 추후 post._id로 수정바람
               )
             })
+            : <div>데이터가 없습니다</div>
           }
         </PostTemplatePostWrapper>
       </PostTemplateRightWrap>
