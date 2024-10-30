456172b088db484281dceb1e24b1af01e24e9b12
w-ho-choo <ettoprivate@gmail.com>
2024-10-30 10:05:42 +0900

    feat: postType에 따른 포스트 리스트 조회 서비스 작성

--- backend/src/services/postService.ts
+++ backend/src/services/postService.ts
@@ -4,3 +4,13 @@ import { Post, IPost } from "../models/postModel";
 export const getPostById = async (postId: string): Promise<IPost | null> => {
   return await Post.findById(postId).populate("authorId"); //populate메서드를 사ㅇㅛㅇ하면 authorId에 저장된 ID를 기반으로 관련된 User문서의 전체 정보를 조회할 수 있음
 };
+
+// postType에 따라 포스트 리스트 조회
+export const getPostList = async (postType: string | undefined) => {
+  const filter: any = {}
+  if (postType){
+    filter.postType = postType;
+  }
+
+  return await Post.find(filter).populate("authorId", "nickname profileImage")
+}
