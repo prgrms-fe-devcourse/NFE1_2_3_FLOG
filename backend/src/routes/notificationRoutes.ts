4963185742d38f1135abc674a0dafcd8d8b8ef78
w-ho-choo <111436278+w-ho-choo@users.noreply.github.com>
2024-11-01 16:14:58 +0900

    Revert "알림 기능 API 구현"

--- backend/src/routes/notificationRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getNotificationList } from "../controllers/notificationController";
-import { authMiddleware } from "../middlewares/authMiddleware";
-
-const router = Router();
-
-router.get('/notifications', authMiddleware, getNotificationList);
-
-export default router;
