f4869311bf9a0200dcf7ba71aa3a9485f57d1f71
MINJU <mjryu00211@gmail.com>
2024-11-05 12:13:16 +0900

    feat: 큐레이션 검색 라우팅 추가

--- backend/src/routes/searchRoutes.ts
+++ backend/src/routes/searchRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getTrendingSearches, searchPosts } from "../controllers/searchController";
+import { getTrendingSearches, searchPosts, searchCurations } from "../controllers/searchController";
 
 const router = Router();
 
 router.get('/search/posts', searchPosts);
+router.get('/search/curations', searchCurations);
 router.get('/search/trending', getTrendingSearches);
 
 export default router;
